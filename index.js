d556721166c2eb7a820521bffc579ae5ee09d5d5
Coline1186 <coline.grosso@neuf.fr>
2024-03-19 11:51:40 +0100

    final

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+let name = ["Van", "Yann", "Curtis", "Charlie"];
+
+console.log(chalk.blue(name[0]));
+console.log(chalk.red(name[1]));
+console.log(chalk.yellow(name[2]));
+console.log(chalk.green(name[3]));
